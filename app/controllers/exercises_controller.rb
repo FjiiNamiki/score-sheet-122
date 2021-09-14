cd4e13b050b14aa1abf47857cb7935771b4e4f40
FjiiNamiki <archerynamiki@gmail.com>
2021-09-15 00:21:31 +0900

    詳細表示のアクション設定

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -3,6 +3,7 @@ class ExercisesController < ApplicationController
     @diary = Diary.find(params[:diary_id])
     @exercise = Exercise.new
   end
+
   def create
     @diary = Diary.find(params[:diary_id])
     @exercise = Exercise.new(exercise_params)
@@ -12,8 +13,29 @@ class ExercisesController < ApplicationController
       render :new
     end
   end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+  def update
+    if @exercise.update(exercise_params)
+      redirect_to action: :show
+    else
+      render action: :edit
+  end
+
   private
+
   def exercise_params
     params.require(:exercise).permit(:weather_id, :distance_id, :sight, :memo).merge(user_id: current_user.id, diary_id: params[:diary_id])
   end
+
+  def set_diary
+    @exercise = Exercise.find(params[:id])
+  end
+
 end
