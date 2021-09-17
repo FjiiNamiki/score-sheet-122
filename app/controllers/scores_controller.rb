9edcb726c8959b7f074e04026845214579a57961
FjiiNamiki <archerynamiki@gmail.com>
2021-09-17 11:48:58 +0900

    newアクションの定義とルーティングの設定

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,8 @@
 class ScoresController < ApplicationController
+  def new
+    @diary = Diary.find(params[:diary_id])
+    @exercise = Exercise.find(params[:exercise_id])
+    @score = Score.new
+  end
+
 end
