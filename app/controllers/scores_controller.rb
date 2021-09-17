c2211a8c69f55562f41ec32dc26b49f446638933
FjiiNamiki <archerynamiki@gmail.com>
2021-09-17 15:32:17 +0900

    スコア表示編集

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,8 +1,17 @@
 class ScoresController < ApplicationController
-  def new
+
+  def create
     @diary = Diary.find(params[:diary_id])
     @exercise = Exercise.find(params[:exercise_id])
-    @score = Score.new
+    @score = Score.new(score_params)
+    if @exercise.save
+      redirect_to "/diaries/#{@diary.id}"
+    else
+      render :new
   end
 
+  private
+  def score_params
+    params.require(:score).permit(:end_id, :first_id, :second_id, :third_id, :fourth_id, :fifth_id, :sixth_id, :total, :ground_total).merge(user_id: current_user.id, diary_id: params[:diary_id], exercise_id: params[:exercise_id])
+  end
 end
