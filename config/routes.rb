2dc663f0a25fb7e4bbcf74f04a29a21caf1fd655
FjiiNamiki <archerynamiki@gmail.com>
2021-09-27 23:39:59 +0900

    ユーザー詳細情報表示機能のコントローラー設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
+  resources :users, only: :show
   resources :diaries do
     resources :exercises do
       resources :scores, only: :create
