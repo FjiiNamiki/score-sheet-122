9c1dbd6c1c457434fdbbc94cda260c943b8f04b1
FjiiNamiki <archerynamiki@gmail.com>
2021-10-05 21:24:59 +0900

    微修正

--- app/javascript/jquery/index.js
+++ app/javascript/jquery/index.js
@@ -1,6 +1,6 @@
 function fadeAnime(){ //さらに動きを関数でまとめる
   $('.fadeInDownTrigger').each(function(){
-    var elemPos = $(this).offset().top-10;
+    var elemPos = $(this).offset().top-50;
     var scroll = $(window).scrollTop();
     var windowHeight = $(window).height();
     if (scroll >= elemPos - windowHeight){   $(this).addClass('animate__animated animate__fadeInDown');
